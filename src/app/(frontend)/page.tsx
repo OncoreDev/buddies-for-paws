75abb45e1801fdb5da9dad73e253e433ea799b7f
TheRealAncientHuman <ancient@hydralaunchpad.com>
2025-07-17 00:10:36 +0800

    feat: Update Home page to fetch and display charities data in CharityMarquee

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,20 +1,21 @@
 import { CharityMarquee } from "@/components/charity-marquee";
-import { DonationStatistics } from "@/components/donation-statstistics";
 import { EventsCarouel } from "@/components/events-carousel";
 import { Hero } from "@/components/hero";
 import { StoreBanner } from "@/components/store-banner";
 import { sanityFetch } from "@/sanity/lib/live";
-import { EVENTS_QUERY } from "@/sanity/lib/queries";
+import { CHARITIES_QUERY, EVENTS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home() {
-  const { data } = await sanityFetch({ query: EVENTS_QUERY });
+  const [events, charities] = await Promise.all([
+    sanityFetch({ query: EVENTS_QUERY }),
+    sanityFetch({ query: CHARITIES_QUERY }),
+  ]);
 
   return (
     <div>
       <Hero />
-      {/* <DonationStatistics /> */}
-      <CharityMarquee />
-      <EventsCarouel events={data} />
+      <CharityMarquee charities={charities.data} />
+      <EventsCarouel events={events.data} />
       <StoreBanner />
     </div>
   );
--- src/components/charity-marquee.tsx
+++ src/components/charity-marquee.tsx
@@ -1,25 +1,28 @@
 import { Marquee } from "@/components/magicui/marquee";
-import { CHARITIES_CONFIG } from "@/lib/charities-config";
-import Image from "next/image";
+import { CHARITIES_QUERYResult } from "@/sanity/types";
 import Link from "next/link";
 
-export function CharityMarquee() {
+export function CharityMarquee({
+  charities,
+}: {
+  charities: CHARITIES_QUERYResult;
+}) {
   return (
     <div className="py-4 lg:py-12">
       <Marquee
         pauseOnHover
         className="[--duration:20s] [--gap:2rem] lg:[--gap:6rem]"
       >
-        {CHARITIES_CONFIG.map((charity) => (
+        {charities.map((charity) => (
           <Link
             key={charity.name}
-            href={charity.url}
+            href={charity.donationUrl || ""}
             target="_blank"
             className="relative size-20 rounded-lg transition-all hover:scale-110 lg:size-32"
           >
-            <Image
-              alt={charity.name}
-              src={charity.image}
+            <img
+              alt={charity.name || ""}
+              src={charity.logoUrl || ""}
               className="h-full w-full object-contain"
             />
           </Link>
