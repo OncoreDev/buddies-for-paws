0e84287c33716487b4d8d40c41ff94ea24df2c7f
TheRealAncientHuman <ancient@hydralaunchpad.com>
2025-10-17 04:14:00 +0800

    feat: add event details and RSVP button to Hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -82,6 +82,73 @@ export function Hero({ items }: { items: HERO_CAROUSEL_QUERYResult }) {
           </div>
 
           <CarouselContent className="-ml-6">
+            <CarouselItem className="flex pl-6">
+              <div className="relative z-0 flex w-full flex-col overflow-hidden bg-black sm:rounded-lg">
+                <img
+                  src={
+                    "https://cdn.sanity.io/images/a5fs5e8w/production/2a6342bd0910ed2650f8207850842aef94893401-1921x1080.png?w=2000&fit=max&auto=format"
+                  }
+                  alt=""
+                  className="absolute inset-0 -z-10 hidden h-full w-full object-cover lg:block"
+                />
+
+                <div className="relative lg:hidden">
+                  <img
+                    src={
+                      "https://cdn.sanity.io/images/a5fs5e8w/production/2a6342bd0910ed2650f8207850842aef94893401-1921x1080.png?w=2000&fit=max&auto=format"
+                    }
+                    alt=""
+                    className="aspect-video w-full object-cover"
+                  />
+
+                  <Button className="absolute right-4 bottom-4 mt-auto ml-auto h-24 w-24 rounded-full pt-2 whitespace-pre-wrap">
+                    <Link
+                      href={"https://luma.com/londonafterhours"}
+                      target="_blank"
+                    >
+                      RSVP Today
+                    </Link>
+                  </Button>
+                </div>
+
+                <div className={cn("flex w-full grow lg:p-8", "text-blue")}>
+                  <div
+                    className={cn(
+                      "flex h-full w-full flex-col gap-6 p-6 pt-8 sm:gap-8 sm:p-8 sm:pt-12 lg:min-h-[600px] lg:w-[500px] lg:rounded-md",
+                      "bg-white sm:bg-white/90",
+                    )}
+                  >
+                    <h2 className="font-cooper text-center text-3xl sm:text-5xl">
+                      Buddies for Paws Fashion Fundraiser
+                    </h2>
+
+                    <div className="mx-auto w-full text-center">
+                      <p>
+                        Buddies for Paws is returning to London on October 21st
+                        2025, for a night of fundraising, fashion & fun!!! Join
+                        us and some very special buddies for an unforgettable
+                        evening. Presented by BONK, Jito, OVHcloud & Solana and
+                        co-hosted by Buddies for Paws, Lissen and Zebu. Expect a
+                        charity fashion show like no other, limited-edition
+                        merch and lots of prizes and surprise. All proceeds from
+                        the event will be donated to Buddies for Paws, and 100%
+                        matched by BONK. Space is limited, secure your spot now
+                      </p>
+                    </div>
+                  </div>
+
+                  <Button className="mt-auto ml-auto hidden h-32 w-32 rounded-full pt-2 text-xl whitespace-pre-wrap lg:flex">
+                    <Link
+                      href={"https://luma.com/londonafterhours"}
+                      target="_blank"
+                    >
+                      RSVP Today
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </CarouselItem>
+
             {items.map((hero, i) => (
               <CarouselItem key={hero.title ?? i} className="flex pl-6">
                 <div
